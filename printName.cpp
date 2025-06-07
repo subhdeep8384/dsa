8824fcc6cfcebad0ced092d05779bed77e9cadc8
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-06-07 08:38:50 +0530

    reverse printing done

--- printName.cpp
+++ printName.cpp
@@ -1,14 +1,30 @@
 #include<iostream>
 #include<string>
 using namespace std ;
-void name(int n  , string nm ){
-    if(n == 0 ){
+// void name(int n  , string nm ){
+//     if(n == 0 ){
+//         return ;
+//     }
+//     cout << nm << n << endl ;
+//     name(n -1  , nm);
+// }
+
+void print(int n , int lim  ){
+    if(n > lim ){
         return ;
     }
-    cout << nm << n << endl ;
-    name(n -1  , nm);
+    cout << n << endl ;
+    print(n + 1 , lim ) ;
+}
+void reversePrint(int n , int lim ){
+    if(lim < n ){
+        return ;
+    }
+    cout << lim << endl ;
+    reversePrint(n , lim - 1) ;
 }
 int main() {
-    name(5 , "subhdeep");
+    // name(500 , "subhdeep");
+    reversePrint(1 , 10 );
     return 0 ;
 }
