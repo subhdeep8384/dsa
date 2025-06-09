b782771684c1cd9ced95f60453f6527bc86b065e
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-06-09 19:05:46 +0530

    all added

--- Hashing/hash.cpp
+++ Hashing/hash.cpp
@@ -1,3 +1,19 @@
 #include<iostream>
 using namespace std ;
-int main() {}
+
+int countNumber(int number , int arr[]  ,int size) {
+    int count = 0 ;
+    for(int i = 0 ; i < size ; i++){
+        if(arr[i] == number ){
+            count++ ;
+        }
+    }
+    return count ;
+} 
+int main() {
+    int number = 1 ;
+    int arr[] = {1 ,1 ,1 ,2 ,4  ,5,1 ,1 ,1 , 1 } ;
+    int size = sizeof(arr) / sizeof(arr[0]) ;
+    int count = countNumber(number , arr ,size);
+    cout << count   << "   "  ;
+}
