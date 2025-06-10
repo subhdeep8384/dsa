43ba256a1887ec0d14e3fe2a20172e4ba5256c62
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-06-10 12:02:44 +0530

    subsequence added

--- subsequence.cpp
+++ subsequence.cpp
@@ -1,7 +1,23 @@
 
 #include<iostream>
+#include<vector>
 using namespace std ;
 
 
-void printSubsequence(){}
-int main(){}
+void printSubsequence(int ind , vector<int> &ds , int arr[] , int n  ) {
+    if(ind == n ) {for(auto it : ds ){cout << it << endl ;}
+        cout << endl ;
+        return ;
+    }
+    ds.push_back(arr[ind]) ;
+    printSubsequence(ind + 1 , ds , arr, n );
+
+    ds.pop_back() ;
+    printSubsequence(ind + 1 , ds , arr, n ) ;
+}
+int main(){
+    int arr[] = {3 , 1, 2 };
+    int n = 3 ;
+    vector<int>ds ;
+    printSubsequence(0 , ds , arr , n ) ;
+}
