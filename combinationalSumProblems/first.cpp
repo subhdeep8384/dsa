6763a396320ce646226fdd03c5c5ba01f482e6ae
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-06-11 12:25:58 +0530

    combinational sum done

---
+++ combinationalSumProblems/first.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std ;
+
+vector<vector<int>> function1(int ind , vector<int> &ds , vector<int> &arr , int target , vector<vector<int>> &ans ){
+    if(ind == arr.size() ){
+        if(target == 0 ){
+            ans.push_back(ds) ;
+        }
+        return ans ;
+    }
+
+    if(arr[ind] <= target ){
+        ds.push_back(arr[ind]) ;
+        function1(ind , ds , arr ,  target - arr[ind] , ans );
+        ds.pop_back() ;
+    }
+    function1(ind + 1 , ds , arr , target , ans   ) ;
+    return ans ;
+ }
+
+int main(){
+
+    vector<int>ds ;
+    vector<int>arr = {2 , 3, 6, 7} ;
+    vector<vector<int>> ans ;
+
+    vector<vector<int>> ansss =  function1(0 , ds , arr , 10 , ans );
+    for (auto it : ansss){
+        for(auto ii : it ){
+            cout << ii << "  ";
+        }
+        cout << endl ;
+    }
+}
