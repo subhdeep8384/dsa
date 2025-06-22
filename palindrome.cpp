522fdaf8f898a3d9eba66a685cc406cb2c6d6683
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-06-22 20:20:49 +0530

    Palindrome done

---
+++ palindrome.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include<vector>
+
+using namespace std ;
+
+bool isPalindrome(string s, int start, int end) {
+    while (start < end) {
+        if (s[start] != s[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+}
+void func(int index , string s , vector<string> &path , vector<vector<string >> &ans ){
+    if(index == s.size() ){
+        ans.push_back(path) ;
+        return ;
+    }
+
+    for(int i = index ; i < s.size() ; i++ ){
+        if(isPalindrome(s , index , i )){
+            path.push_back(s.substr(index , i - index + 1)) ;
+            func(i + 1 , s, path , ans ) ;
+            path.pop_back() ;
+        }
+    }
+} 
+vector<vector<string>> palindromeStrings(string s ){
+    vector<vector<string>> ans ;
+    vector<string> path ;
+
+    func(0 , s , path , ans ) ;
+    return ans ;
+}
+
+
+
+
+
+int main() {
+    string s = "aabb" ;
+    vector<vector<string >> ans = palindromeStrings(s) ;
+
+    for(auto it : ans){
+        for(auto i : it ){
+            cout << i << "  " ;
+        }
+        cout << endl ;
+    }
+}
