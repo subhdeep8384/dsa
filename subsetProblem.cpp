6d556a9b9fbc07b76f9247ee41f2947e1fe833b1
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-06-23 19:26:30 +0530

    subset problem done

---
+++ subsetProblem.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std ;
+
+void subset( int index , vector<int> &arr , int sum ,vector<int> &ans   ){
+    
+    
+    if  ( index ==  arr.size() ){
+        ans.push_back(sum) ;
+    
+        return ;
+    }
+    subset(index + 1 ,  arr , sum + arr[index] , ans) ;
+    // ans.pop_back() ;
+    subset(index + 1 , arr ,sum  , ans) ;
+
+    // return ans ;
+}
+
+
+int main() {
+    vector<int >arr = {3 ,1 , 2} ;
+    vector<int> ans  ;
+    subset(0 ,  arr , 0  , ans );
+
+    for(auto it : ans ){
+        cout << it << endl ;
+    }
+}
