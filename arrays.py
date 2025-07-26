f6f04c11424af0a3f13bc7b63af139f7858c0d7d
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-07-26 09:57:19 +0530

    m

--- arrays.py
+++ arrays.py
@@ -1,10 +1,20 @@
 arr = [1 , 2, 3, 4, 5, 6, 7, 8]  
 largest = arr[0]
+smallest = arr[0]
 
 
 for  i in range(len(arr) ) :
     if(arr[i] > largest) :
-        largest = arr[i] ;
+        largest = arr[i] 
 
-
+arr.sort() 
+print("The largest elemet after sorting is : ", arr[len(arr) - 1])  
 print("The largest elemet is : " , largest )
+
+
+for i in range(len(arr)):
+    if(arr[i] < smallest):
+        smallest = arr[i]
+
+print("smallest element is ::" , smallest )
+print("smallest element by sort is " , arr[0]); 
