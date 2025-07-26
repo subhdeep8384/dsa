daa5f36ec6723c83ce0702eaed9fe6ee2a4cf0de
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-07-26 10:35:20 +0530

    a

--- arrays/array.py
+++ arrays/array.py
@@ -8,14 +8,14 @@ for i in range (len(a) ):
     if(a[i] > largest ):
         largest = a[i] 
 
-# print("the lagest elemet is : " , largest )
+print("the lagest elemet is : " , largest )
 
 
 for i in range (len(a)) :
     if(a[i] > sLargest and a[i] < largest):
         sLargest = a[i]
 
-print("second Largest element is " , sLargest)
+print("second Largest element is " , sLargest) 
 
 
 print("Another way is ")
@@ -29,3 +29,16 @@ for i in range (len(a) - 2 , -1 , -1 ) :
 
 print("Second largest element by sorting is " , sLargest)
 
+b = np.array([5,1,2,3,6,111,5,47]) 
+
+l = b[0] 
+sl = -1 
+print(len(b))
+for i in range(1 , len(b)):
+    if(b[i] > l):
+        sl = l 
+        l = b[i]
+    elif b[i] > sl and b[i] < l :
+        sl = b[i]
+
+print("Second largest is" , sl) 
