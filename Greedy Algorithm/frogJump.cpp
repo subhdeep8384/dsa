12280147bd44e5a08f622e0d10dcebeb98ca8a5f
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-09-03 20:55:36 +0530

    frog jump question done

---
+++ Greedy Algorithm/frogJump.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int frogJump(vector<int> arr)
+{
+    int n = arr.size();
+    int maxInt = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+       if( i > maxInt ) return -1 ;
+       maxInt = max(maxInt , i + arr[i] ) ;
+    }
+    return 1;
+}
+
+int main() {
+    vector<int> arr = {0, 0, 0, 0, 4};
+    int result = frogJump(arr);
+    cout << result;
+    return 0;
+}
