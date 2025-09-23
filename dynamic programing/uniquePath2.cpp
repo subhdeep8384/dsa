4a4bf6b8f79d1bdd9063adb79e26037a8f79d5c8
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-09-23 22:49:24 +0530

    part-2 unique path done

--- dynamic programing/uniquePath2.cpp
+++ dynamic programing/uniquePath2.cpp
@@ -16,6 +16,42 @@ int fn(int i , int j , vector<vector<int>> &grid) {
     return up + left ;
 }
 
+int MEMO(int i , int j , vector<vector<int>> &grid , vector<vector<int>> &dp) {
+    if(i == 0 && j == 0 ){
+        return 1 ;
+    }
+    if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()){
+        return 0 ;
+    }
+
+    if(i >= 0 && j >= 0 && grid[i][j] == -1 )return 0 ;
+    if(dp[i][j] != -1 )return dp[i][j] ;
+    int up = fn(i -1 , j , grid) ;
+    int left = fn(i , j - 1 , grid);
+
+    return dp[i][j] =  up + left ;
+}
+
+int Tabulation(int i , int j , vector<vector<int>>&grid  , vector<vector<int>> &dp ){
+    dp[0][0] = 1 ;
+    for(int l = 0 ; l < grid[0].size() ; l++ ){
+        for(int k =  0 ; k < grid.size() ; k++ ){
+            if(grid[i][j] == -1 ) dp[i][j] = 0  ;
+            else if(i == 0 && j == 0  ) return dp[0][0] = 1 ;
+            else{
+                int up , left ;
+                if(i > 0 ){
+                    up = dp[i - 1 ][j] ;
+                }
+                if(j > 0 ){
+                    left = dp[i][j - 1] ;
+                }
+                dp[i][j] = up + left ;
+            }
+        }
+    }
+    return dp[i][j] ;
+}
 int main() {
 
 } 
