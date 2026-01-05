a6966c67ae75da249553c866158c2848264ca4fe
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2026-01-05 23:12:44 +0530

    hashmap done

---
+++ Hashing/hashmapp.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<map>
+using namespace std ;
+
+int main (){
+    int n ; 
+    cout << "Enter the number of elements : " ;
+    cin >> n ; 
+    int arr[n] ;
+    cout << "Enter the elements : " ;
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i] ;
+    }
+
+    // precompute 
+
+
+    map<int , int > m ; 
+    for(int i = 0 ; i < n ; i++ ){
+        m[arr[i]]++ ;
+    }
+
+    for(auto it : m ){
+        cout << it.first << "->"<< it.second << endl ; 
+    }
+    int q ; 
+    cin >> q  ;
+    cout << "Enter the queries : " ;
+    while (q-- ){
+        int number ; 
+        cin >> number ;
+        cout << m[number] << endl ;
+    }
+    return 0 ; 
+}
